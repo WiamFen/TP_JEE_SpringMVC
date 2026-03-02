03b8f5e633e1bf573d315f527cc98890b5888f3b
WiamFen <wfenjiro@gmail.com>
2026-03-02 22:27:29 +0000

    Initialisation du projet

---
+++ src/main/java/ma/enset/tp2_springmvc/Tp2SpringMvcApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.tp2_springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp2SpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp2SpringMvcApplication.class, args);
+	}
+
+}
