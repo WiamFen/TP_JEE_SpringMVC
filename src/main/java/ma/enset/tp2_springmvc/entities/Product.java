13f1cafb7b6b8f4637ad7198deaf4683c5c380d0
WiamFen <wfenjiro@gmail.com>
2026-03-02 22:31:55 +0000

    Création de l’entité JPA Product

---
+++ src/main/java/ma/enset/tp2_springmvc/entities/Product.java
@@ -0,0 +1,28 @@
+package ma.enset.tp2_springmvc.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+
+public class Product {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotEmpty
+    private String name;
+    @Min(0)
+    private Double price;
+    @Min(1)
+    public Double quantity;
+}
